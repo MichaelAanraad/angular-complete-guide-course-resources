6d8c4ea810f4814d96f02fc3dad875c45b4a43dd
Michael Aanraad <michaelaanraad@gmail.com>
2025-09-09 13:10:55 +0400

    apply input.required signals

--- code-snapshots/02-essentials/04-used-cli-to-create-user-cmp/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/04-used-cli-to-create-user-cmp/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import {Component, computed, signal} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users'
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import {Component, computed, Input, input} from '@angular/core'
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,11 +6,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar()
+  })
+
+  // get imagePath(){
+  //   return 'assets/users/' + this.avatar;
+  // }
+  onSelectUser(){}
 }
--- code-snapshots/02-essentials/11-adding-cmp-inputs/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/11-adding-cmp-inputs/src/app/user/user.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-
 @Component({
   selector: 'app-user',
   standalone: true,
