ae10c3ac6d647be316e525afa33084c8aaa1d9e5
Michael Aanraad <michaelaanraad@gmail.com>
2025-09-09 15:41:12 +0400

    Create tasks component

--- code-snapshots/02-essentials/04-used-cli-to-create-user-cmp/src/app/app.component.ts
+++ code-snapshots/02-essentials/04-used-cli-to-create-user-cmp/src/app/app.component.ts
@@ -3,21 +3,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import {UserComponent} from "./user/user.component";
 import {DUMMY_USERS} from "./dummy-users";
+import {TasksComponent} from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    UserComponent
+    UserComponent,
+    TasksComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUser: { id: string; name: string; avatar: string; } | undefined;
 
   onSelectUser(id : string){
-    console.log('selected user with id ' + id)
+    this.selectedUser = DUMMY_USERS.find(user => user.id == id);
   }
 }
